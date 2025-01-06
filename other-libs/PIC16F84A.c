aa2965abbdbd7d29fab6fa2adba94e181056f3c8
Renato Lobo <renatolobojr@gmail.com>
2025-01-06 00:02:30 -0300

    separando as tentaivas de libs para mexer em outro momento

---
+++ other-libs/PIC16F84A.c
@@ -0,0 +1,12 @@
+
+#include "PIC16F84A.h"
+
+const double RAIO = 2.5;
+const double LARGURA = 1.2;
+
+const double MEU_PI = 3.14;
+
+//double area_circulo(Circulo c)
+//{
+    //return c.raio * c.raio * MEU_PI;
+//}
---
+++ other-libs/PIC16FXXX.c
@@ -0,0 +1,11 @@
+#include "PIC16FXXX.h"
+
+//const double RAIO = 2.5;
+//const double LARGURA = 1.2;
+
+//const double MEU_PI = 3.14;
+
+//double area_circulo1(Circulo c)
+//{
+    //return c.raio * c.raio * MEU_PI;
+//}
